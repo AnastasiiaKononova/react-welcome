2855bde36493e4c5988c2efa26bb3ab42919888d
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-10-24 15:23:33 +0300

    functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-const App = function (props) {
+function App (props) {
+  const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 18
+  }
+  
+  // const h1 = React.createElement('h1', {}, `Hello, ${user.firstName}`);
+  // return h1;
 
-  // const button = React.createElement('button, {}, X);
-//  const li = React.createElement('li', {}, 'item!', button);
-// const ul = React.createElement('ul', {}, li);
-
-// const h1 = <h1>Hello, my dear React.js</h1>
-//   return h1
-
-return (<ul>
-            <li>item 
-              <button>X</button>
-            </li>
-          </ul>)
+  return <h1>Hello, {user.firstName}</h1>// JSX-синтаксис
 }
 
 export default App;
@@ -20,6 +17,15 @@ export default App;
 /*
 Компоненти в React - це блок коду (функція, наприклад), який створює і повертає react-елементи
 Компоненти бувають двох типів:
+
 - Функціональні - це звичайна JS-функція, яка повертає створений react-елемент
+
 - Класові - це клас, який наслідується від React.Component і описувати шаблон компоненти, яка має бути в інтерфейсі
+*/
+
+/*
+Рендер - процес відмальовування (отрисовки) елементів на сторінці браузера.
+(re-render - перемальовування, оновлення)
+
+Браузер оновлює елементи на екрані (ререндерить сторінку) в той момент, коли стек пустий
 */
