c16c960ece756b89a65503548bd361bcd8e6dfbd
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-11-06 14:15:13 +0200

    Counter Child->Parent task resolve

---
+++ src/components/Counter/CounterBtns/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const CounterBtns = (props) => {
+    return (
+        <div>
+            <button onClick={props.decrement}>-</button>
+            <button onClick={props.increment}>+</button>
+        </div>
+    );
+}
+export default CounterBtns;
