c403dedb2c338122bf0edc04651c87325ffbac34
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-11-11 14:59:36 +0200

    animation add

--- src/components/Aloha/index.js
+++ src/components/Aloha/index.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import "animate.css";
 
-
- class Aloha extends React.Component {
+class Aloha extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        isGreeting: true
-    }
+      isGreeting: true,
+    };
   }
-  
+
   clickHandler = () => {
     this.setState({
-        isGreeting: !this.state.isGreeting
-    })
+      isGreeting: !this.state.isGreeting,
+    });
+  };
+
+  render() {
+    const hreetingText = this.state.isGreeting ? "Hello" : "Bye";
+    return (
+      <h2
+        className="red-big-text animate__animated animate__bounceIn"
+        onClick={this.clickHandler}
+      >
+        {hreetingText}, {this.props.name}
+      </h2>
+    );
   }
-  
-  
-  
-  
-    render() {
-        const hreetingText = this.state.isGreeting ? 'Hello' : 'Bye';
-        return (<h2 
-            className="red-big-text"
-            onClick={this.clickHandler}
-            >
-                {hreetingText}, {this.props.name}
-                </h2>
-        )
-    }
 }
 
 export default Aloha;
