a9390a58432240a8679dc33adfe43f8d8d2585df
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-11-21 11:45:21 +0200

    render prop task resolve

--- src/components/PhoneLoader/index.js
+++ src/components/PhoneLoader/index.js
@@ -32,11 +32,7 @@ class PhoneLoader extends Component {
     }
     
     render() {
-        return (
-            <ol>
-                {this.state.phones.map(p => <li key={p.id}>{p.brand} - {p.model}</li>)}
-            </ol>
-        );
+        return this.props.children(this.state)
     }
 }
 
