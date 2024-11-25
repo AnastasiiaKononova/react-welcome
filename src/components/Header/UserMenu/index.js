8b2fb8c3f10cb61e55d7050ae9e197aa48111069
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-11-25 13:36:16 +0200

    ThemeContext task resolve

--- src/components/Header/UserMenu/index.js
+++ src/components/Header/UserMenu/index.js
@@ -1,21 +1,26 @@
 import React from "react";
-import ContextObj from "../../../contexts/UserContext";
 import UserContext from "../../../contexts/UserContext";
+import ThemeContext from "../../../contexts/ThemeContext";
 
 const UserMenu = () => {
   return (
-    <UserContext.Consumer>
-      {([user, logOut]) => {
-        console.log(user);
-        return (
-          <div style={{ width: "200px", border: "2px solid blue" }}>
-            <p>
-              {user.firstName} {user.lastName}
-            </p>
-          </div>
-        );
-      }}
-    </UserContext.Consumer>
-  );
-};
+    <ThemeContext.Consumer>
+        {([theme, setTheme]) => {
+            return (
+                <UserContext.Consumer>
+                {([user, logOut]) => {
+                    console.log(user);
+                    return (
+                        <div style={{width: '200px', border: '2px solid blue'}}>
+                        <p>{user.firstName} {user.lastName}</p>
+                        <p>Theme: {theme}</p>
+                        </div>)
+                }}
+                </UserContext.Consumer> 
+            )
+        }}
+       </ThemeContext.Consumer>       
+    );
+}
+
 export default UserMenu;
