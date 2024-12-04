db1fd3c0b5b332aa535ef9d6419542477ff12c41
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-12-04 18:12:46 +0200

    state child props

--- src/components/NewCounter/index.js
+++ src/components/NewCounter/index.js
@@ -1,11 +1,12 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import InnerCompo from "./innerComp";
 
 const NewCounter = () => {
   // функціональна компонента - хук використовувати можна
-    // ось це - найвищий рівень
-    const [count, setCount] = useState(0);
-// На відміну від класового стану, тут state - не об'єкт, а той тип даних, який ви туди поклали
-    /*
+  // ось це - найвищий рівень
+  const [count, setCount] = useState(0);
+  // На відміну від класового стану, тут state - не об'єкт, а той тип даних, який ви туди поклали
+  /*
     if() {
         // ось це вже не найвищий рівень
     } else{
@@ -14,18 +15,22 @@ const NewCounter = () => {
         //це теж не найвищий рівень
     }
     */
-    const increment = () => {
-        setCount(count+1)
-    }
-    const decrement = () => {
-        setCount(count-1)
-    }
-    return (
-        <div>
-           <h2>{count}</h2> 
-           <button onClick={increment}>+</button>
-           <button onClick={decrement}>-</button>
-        </div>
-    );
-}
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
+  console.log("батьківська оновилась!");
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <InnerCompo callbackFromParent={setCount} />
+    </div>
+  );
+};
 export default NewCounter;
