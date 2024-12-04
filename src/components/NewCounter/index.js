8cba86ad10f6551599d66d1f19121b77773e9a4c
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-12-04 17:59:48 +0200

    naming

--- src/components/NewCounter/index.js
+++ src/components/NewCounter/index.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 const NewCounter = () => {
   // функціональна компонента - хук використовувати можна
     // ось це - найвищий рівень
-    const [state, setState] = useState(0);
+    const [count, setCount] = useState(0);
 // На відміну від класового стану, тут state - не об'єкт, а той тип даних, який ви туди поклали
     /*
     if() {
@@ -15,14 +15,14 @@ const NewCounter = () => {
     }
     */
     const increment = () => {
-        setState(state+1)
+        setCount(count+1)
     }
     const decrement = () => {
-        setState(state-1)
+        setCount(count-1)
     }
     return (
         <div>
-           <h2>{state}</h2> 
+           <h2>{count}</h2> 
            <button onClick={increment}>+</button>
            <button onClick={decrement}>-</button>
         </div>
