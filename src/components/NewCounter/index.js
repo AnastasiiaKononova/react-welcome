61129894e572f9c8bb93b6644fbe3039f205e326
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-12-04 18:58:36 +0200

    Why not complicated state

--- src/components/NewCounter/index.js
+++ src/components/NewCounter/index.js
@@ -1,36 +1,64 @@
 import React, { useState } from "react";
-import InnerCompo from "./innerComp";
+
 
 const NewCounter = () => {
   // функціональна компонента - хук використовувати можна
   // ось це - найвищий рівень
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState('');
+  const [state, setState] = useState ({
+    count: 0,
+    step: ''
+  })
+  // const [step, setStep] = useState('');
   // На відміну від класового стану, тут state - не об'єкт, а той тип даних, який ви туди поклали
  
+const newIncrement = () => {
+  setState ({
+    ...state,
+    count: state.count + 1
+  })
+}
 
+const changeStepHandler = ({target: {value}}) => {
+  setState({
+    ...state,
+    step: value
+  })
+}
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount((prevState) => prevState - 1);
-  };
+  // const increment = () => {
+  //   setCount(count + 1);
+  // };
+  // const decrement = () => {
+  //   setCount((prevState) => prevState - 1);
+  // };
 
-  const changeHandler = ({target: {value}}) => {
-    setStep(value)
-  }
+  // const changeHandler = ({target: {value}}) => {
+  //   setStep(value)
+  // }
 
-  console.log("батьківська оновилась!");
+  // console.log("батьківська оновилась!");
 
   return (
     <div>
-      <h2>{count}</h2>
-      <input type="number" name="step" value={step} onChange={changeHandler}></input>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <InnerCompo callbackFromParent={setCount} />
+      <h2>{state.count}</h2>
+      <input type="number" name="step" value={state.step} onChange={changeStepHandler}></input>
+      <button onClick={newIncrement}>+</button>
+      {/* <button onClick={decrement}>-</button> */}
     </div>
   );
 };
+
 export default NewCounter;
+
+/*
+this.setState (функція для зміни стану класової компоненти)
+
+{новий стан} -> {...state, ...новий стан}
+ 
+this.setState({
+        user: {}
+      })  -> {...this.state, ...новий юзер}
+
+      setState (хук)
+{новий стан} -> {новий стан}
+*/
